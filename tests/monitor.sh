b3c97e38657a4afe1bdbf16b7903dc02dc2d4684
Andre Heinemans <andre.heinemans@nxp.com>
2026-07-13 12:26:05 +0200

    adapt monitor to use leds component instead of driving gpio directly

--- tests/monitor.sh
+++ tests/monitor.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
-RED=11
-GREEN=12
-BLUE=13
+echo none > /sys/class/leds/red:status/trigger
+echo none > /sys/class/leds/green:status/trigger
+echo none > /sys/class/leds/blue:status/trigger
+
+RED_BRIGHTNESS=/sys/class/leds/red:status/brightness
+GREEN_BRIGHTNESS=/sys/class/leds/green:status/brightness
+BLUE_BRIGHTNESS=/sys/class/leds/blue:status/brightness
+
+ON=1
+OFF=0
 
 if [ -n "$1" ]; then
     if [ "$1" = "off" ]; then
-        gpioset gpiochip5 $RED=0
-        gpioset gpiochip5 $GREEN=0
-        gpioset gpiochip5 $BLUE=0
+        echo $OFF > $RED_BRIGHTNESS
+        echo $OFF > $GREEN_BRIGHTNESS
+        echo $OFF > $BLUE_BRIGHTNESS
         exit 0
     fi
 fi
@@ -22,19 +29,19 @@ while true; do
 
     if [ "$STATE" = true ]; then
         if echo $OUTPUT | tr ' ' '\n' | grep -qv '^active$'; then
-            gpioset gpiochip5 $RED=1
-            gpioset gpiochip5 $GREEN=0
-            gpioset gpiochip5 $BLUE=0
+            echo $ON > $RED_BRIGHTNESS
+            echo $OFF > $GREEN_BRIGHTNESS
+            echo $OFF > $BLUE_BRIGHTNESS
         else
-            gpioset gpiochip5 $RED=0
-            gpioset gpiochip5 $GREEN=1
-            gpioset gpiochip5 $BLUE=0
+            echo $OFF > $RED_BRIGHTNESS
+            echo $ON > $GREEN_BRIGHTNESS
+            echo $OFF > $BLUE_BRIGHTNESS
         fi
         STATE=false
     else
-        gpioset gpiochip5 $RED=0
-        gpioset gpiochip5 $GREEN=0
-        gpioset gpiochip5 $BLUE=0
+        echo $OFF > $RED_BRIGHTNESS
+        echo $OFF > $GREEN_BRIGHTNESS
+        echo $OFF > $BLUE_BRIGHTNESS
         STATE=true
     fi
     sleep 0.5
